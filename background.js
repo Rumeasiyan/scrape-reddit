10830ea8aeae577f4dd87218aaa6fc237a20634c
Suseenthiran Arulraj Rumeasiyan <srumeasiyan@gmail.com>
2024-03-05 11:18:08 +0530

    debugs errors

--- background.js
+++ background.js
@@ -1,6 +1,9 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (
+        changeInfo &&
         changeInfo.status === "complete" &&
+        tab &&
+        tab.url &&
         tab.url.startsWith("https://www.reddit.com/")
     ) {
         chrome.scripting.executeScript({
@@ -14,7 +17,20 @@ let redditDataArray = [];
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.redditData) {
-        redditDataArray = redditDataArray.concat(message.redditData);
+        // Convert the array to JSON string
+        const jsonData = JSON.stringify(message.redditData, null, 2);
+
+        // Create a Blob containing the JSON data
+        const blob = new Blob([jsonData], { type: "application/json" });
+
+        // Create a URL for the Blob
+        const blobUrl = URL.createObjectURL(blob);
+
+        // Open the URL in a new tab
+        chrome.tabs.create({ url: blobUrl });
+
+        // Note: The Blob URL will be automatically revoked once the tab is closed
+        console.log("redditData");
     }
 });
 
