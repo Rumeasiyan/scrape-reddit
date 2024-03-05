55d833a5d302843b312475c718e1917cc57263c6
Suseenthiran Arulraj Rumeasiyan <srumeasiyan@gmail.com>
2024-03-05 11:02:38 +0530

    modifes background.js

--- background.js
+++ background.js
@@ -1,9 +1,24 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && tab.url.startsWith('https://www.reddit.com/')) {
-      chrome.scripting.executeScript({
-        target: { tabId: tabId },
-        files: ['content.js']
-      });
+    if (
+        changeInfo.status === "complete" &&
+        tab.url.startsWith("https://www.reddit.com/")
+    ) {
+        chrome.scripting.executeScript({
+            target: { tabId: tabId },
+            files: ["content.js"],
+        });
     }
-  });
-  
+});
+
+let redditDataArray = [];
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.redditData) {
+        redditDataArray = redditDataArray.concat(message.redditData);
+    }
+});
+
+// Function to retrieve the scraped data array
+function getRedditDataArray() {
+    return redditDataArray;
+}
